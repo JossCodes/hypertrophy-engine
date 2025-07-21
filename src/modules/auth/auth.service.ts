43bcda7ef8bf6f0cdd8e02f1a18cde932dc17c8a
Josue Cruz <josuecruz@Josues-MacBook-Pro.local>
2025-07-21 16:28:53 -0600

    chore(auth): scaffold auth.service with register and login signatures

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { LoginInput, RegisterInput } from "./auth.validation.js";
+
+export async function registerUser(input: RegisterInput) {
+  // TODO: implement user registration
+}
+
+export async function loginUser(input: LoginInput) {
+  // TODO: implement user login
+}
